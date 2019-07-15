bd927e0bf1b132d29e0780c46c1c417ed36e0fb3
Unknown <geraldo.rodriguesveloso111@gmail.com>
2019-07-15 19:11:30 +0100

    Working test back

---
+++ backend/src/test/java/main/service/GymServiceTest.java
@@ -0,0 +1,44 @@
+package main.service;
+
+import main.dao.GymDao;
+import main.model.Gym;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class GymServiceTest {
+    @Autowired
+    GymDao gymDao;
+    @Autowired
+    GymService gymService;
+
+
+    @Test
+    public void givenExistGym_WhenGetGym_thenReturnGym() {
+        Gym gym = new Gym();
+        gym.setName("gym");
+        gym.setId(1);
+        gymDao.save(gym);
+
+        Optional<Gym> gym1 = gymService.getGym(1);
+
+        assertTrue(gym1.isPresent());
+    }
+
+    @Test
+    public void givenNotExistGym_WhenGetGym_thenReturnGym() {
+        Optional<Gym> gym = gymService.getGym(2);
+        assertFalse(gym.isPresent());
+    }
+}
